c7cf7ecb598a58513b5027f09f6f60cd1226847f
AEMS1 <aems91806@gmail.com>
2025-08-05 07:45:49 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,22 +1,26 @@
-const tokens = [
+const tokenList = [
   {
+    name: "BNB",
     symbol: "BNB",
-    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // Native BNB
+    address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // برای BNB native
     decimals: 18
   },
   {
+    name: "Legend Coin",
+    symbol: "LEG",
+    address: "0xa3e97bfd45fd6103026fc5c2db10f29b268e4e0d",
+    decimals: 18
+  },
+  {
+    name: "USDT",
     symbol: "USDT",
-    address: "0x55d398326f99059fF775485246999027B3197955", // USDT on BSC
+    address: "0x55d398326f99059fF775485246999027B3197955", // USDT BEP-20
     decimals: 18
   },
   {
+    name: "BUSD",
     symbol: "BUSD",
-    address: "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD on BSC
-    decimals: 18
-  },
-  {
-    symbol: "LEGEND",
-    address: "0x67bdC53777E0895a1Af6682d6A3E9b15636B2c70", // Legend token (فرضی از صرافی شما)
+    address: "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD BEP-20
     decimals: 18
   }
 ];
