4ea8cb35b13933e2b852fbf9de9b31200730b6fa
AEMS1 <aems91806@gmail.com>
2025-08-05 08:50:08 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,87 +1,58 @@
-// ABI برای توکن‌های استاندارد BEP20
-const erc20ABI = [
-  {
-    constant: true,
-    inputs: [],
-    name: "decimals",
-    outputs: [{ name: "", type: "uint8" }],
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [{ name: "", type: "string" }],
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_spender", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [{ name: "success", type: "bool" }],
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "balance", type: "uint256" }],
-    type: "function",
-  }
-];
-
-// ABI مربوط به قرارداد توزیع پاداش شما
-const rewardDistributorABI = [
-  {
-    inputs: [],
-    name: "claimReward",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
-  }
-];
-
-// ABI روتر PancakeSwap (مهم برای سواپ)
+// ABI مربوط به PancakeSwap Router V2 (برای انجام سواپ‌ها)
 const pancakeRouterABI = [
   {
-    name: "swapExactETHForTokens",
-    type: "function",
-    stateMutability: "payable",
-    inputs: [
-      { name: "amountOutMin", type: "uint256" },
-      { name: "path", type: "address[]" },
-      { name: "to", type: "address" },
-      { name: "deadline", type: "uint256" }
+    "inputs": [
+      { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
+      { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
     ],
-    outputs: [{ name: "amounts", type: "uint256[]" }]
+    "name": "swapExactTokensForTokens",
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    name: "swapExactTokensForETH",
-    type: "function",
-    stateMutability: "nonpayable",
-    inputs: [
-      { name: "amountIn", type: "uint256" },
-      { name: "amountOutMin", type: "uint256" },
-      { name: "path", type: "address[]" },
-      { name: "to", type: "address" },
-      { name: "deadline", type: "uint256" }
+    "inputs": [
+      { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
     ],
-    outputs: [{ name: "amounts", type: "uint256[]" }]
+    "name": "swapExactETHForTokens",
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    name: "swapExactTokensForTokens",
-    type: "function",
-    stateMutability: "nonpayable",
-    inputs: [
-      { name: "amountIn", type: "uint256" },
-      { name: "amountOutMin", type: "uint256" },
-      { name: "path", type: "address[]" },
-      { name: "to", type: "address" },
-      { name: "deadline", type: "uint256" }
+    "inputs": [
+      { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
+      { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
     ],
-    outputs: [{ name: "amounts", type: "uint256[]" }]
+    "name": "swapExactTokensForETH",
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+// ABI مربوط به قرارداد پاداش RewardDistributor
+const rewardDistributorABI = [
+  {
+    "inputs": [],
+    "name": "claimReward",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
 ];
